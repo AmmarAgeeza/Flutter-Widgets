eef37a7357f563b32994ea8fb6db4ce49a90b598
AmmarAgeeza <ammarfathy516@gmail.com>
2023-03-13 17:25:45 +0200

    initial

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Widgets',
+      home: FlutterWidgets(),
+    );
+  }
+}
+
+class FlutterWidgets extends StatelessWidget {
+  const FlutterWidgets({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
